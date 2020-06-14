792e449f1fd6bcd87722a86d68f350f4ef8dd7fd
Filip Sobalski <pinkeen@gmail.com>
2020-06-14 08:27:44 +0200

    Fix warmup service - depenencies, script, ...

--- roles/cs.magento-node-warmup/templates/magento-warm-up.sh
+++ roles/cs.magento-node-warmup/templates/magento-warm-up.sh
@@ -7,10 +7,12 @@ WARMUP_BASE_URL="http://localhost:{{ magento_node_warmup_local_port }}"
 WARMUP_URLS='{{ magento_node_warmup_url_paths | join(' ') }}'
 
 for URL in $WARMUP_URLS ; do
+    # Never fail, we don't want unhealthy degraded system state if this fails
     curl \
         -Lsk \
         -w "[$WARMUP_HOST_FAKE] (%{time_starttransfer}s) [%{http_code}<-] %{url_effective}\n" \
         -H "Host: $WARMUP_HOST_FAKE" \
         -o /dev/null \
-            "$WARMUP_BASE_URL$URL"
+            "$WARMUP_BASE_URL$URL" \
+        || true
 done
