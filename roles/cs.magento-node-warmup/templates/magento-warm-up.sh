48137245e96348976061ff10f73e88005478d685
Filip Sobalski <pinkeen@gmail.com>
2020-06-16 18:36:39 +0200

    Run the same cs.magento-configure play for raccoon, just skipping the steps not necessary in development mode

--- roles/cs.magento-node-warmup/templates/magento-warm-up.sh
+++ roles/cs.magento-node-warmup/templates/magento-warm-up.sh
@@ -6,13 +6,24 @@ WARMUP_HOST_FAKE="{{ magento_node_warmup_host_fake }}"
 WARMUP_BASE_URL="http://localhost:{{ magento_node_warmup_local_port }}"
 WARMUP_URLS='{{ magento_node_warmup_url_paths | join(' ') }}'
 
+echo -e '[INFO] Started System Boot Magento Full Page Cache Warmup!\n' | wall -n
+
+echo "[$(date -Iseconds)] [WARMUP] Started" > /tmp/magento-warm-up.log
+
 for URL in $WARMUP_URLS ; do
     # Never fail, we don't want unhealthy degraded system state if this fails
     curl \
         -Lsk \
-        -w "[$WARMUP_HOST_FAKE] (%{time_starttransfer}s) [%{http_code}<-] %{url_effective}\n" \
+           -w "[$(date -Iseconds)] [%{http_code}] (%{time_starttransfer}s) GET %{url_effective}\n" \
         -H "Host: $WARMUP_HOST_FAKE" \
         -o /dev/null \
             "$WARMUP_BASE_URL$URL" \
+        2>&1 \
+	| tee -a /tmp/magento-warm-up.log \
         || true
 done
+
+echo "[$(date -Iseconds)] [WARMUP] Finished" >> /tmp/magento-warm-up.log
+
+echo -e '[INFO] System Boot Magento Full Page Cache Warmup has been completed\n\n$(cat /tmp/magento-warm-up.log)\n' | wall -n
+
