d913284a40d9165668cc7fad67bc1dd7fbec4ea3
Piotr Rogowski <piotr.rogowski@creativestyle.pl>
2020-12-18 15:54:08 +0100

    Use IMDSv2 api

---
+++ roles/cs.mageops-cli/files/libaws.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+aws__token=""
+aws__token_expire=0
+aws__session_time=60
+
+aws::_refresh_token() {
+    local now
+
+    now="$(date '+%s')"
+    if [ "$now" -ge "$aws__token_expire" ];then
+        aws__token="$(curl -sf -X PUT  -H "X-aws-ec2-metadata-token-ttl-seconds: $aws__session_time" "http://169.254.169.254/latest/api/token")"
+        aws__token_expire="$(( now + aws__session_time))"
+    fi
+}
+
+aws::_curl() {
+    aws::_refresh_token
+    curl -Lsf -H "X-aws-ec2-metadata-token: $aws__token" "$@"
+}
+
+aws::current_instance_id() {
+    aws::_curl http://instance-data/latest/meta-data/instance-id || return 1
+}
+
+aws::get_current_region() {
+    aws::_curl http://instance-data/latest/meta-data/placement/availability-zone | sed 's/[a-z]$//' || return 1
+}
--- roles/cs.mageops-cli/files/libmageops.bash
+++ roles/cs.mageops-cli/files/libmageops.bash
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-mageops::get_current_instance_id() {
-    curl -Lsf http://instance-data/latest/meta-data/instance-id || return 1
-}
-
-mageops::get_current_region() {
-    curl -Lsf http://instance-data/latest/meta-data/placement/availability-zone | sed 's/[a-z]$//' || return 1
-}
-
 mageops::get_ec2_tag_value() {
     local region=$1
     local instance_id=$2
@@ -25,8 +17,8 @@ mageops::get_tag_value() {
     local instance_id
     local found_value
     local region
-    region="$(mageops::get_current_region)"
-    instance_id="$(mageops::get_current_instance_id)"
+    region="$(aws::get_current_region)"
+    instance_id="$(aws::current_instance_id)"
     found_value="$(mageops::get_ec2_tag_value "$region" \
         "$instance_id" "$tag_name")"
 
@@ -39,8 +31,8 @@ mageops::is_tag_exists() {
     local instance_id
     local found_value
     local region
-    region="$(mageops::get_current_region)"
-    instance_id="$(mageops::get_current_instance_id)"
+    region="$(aws::get_current_region)"
+    instance_id="$(aws::current_instance_id)"
     found_value="$(mageops::get_ec2_tag_value "$region" \
         "$instance_id" "$tag_name")"
 
@@ -57,8 +49,8 @@ mageops::assert_tag_value() {
     local instance_id
     local found_value
     local region
-    region="$(mageops::get_current_region)"
-    instance_id="$(mageops::get_current_instance_id)"
+    region="$(aws::get_current_region)"
+    instance_id="$(aws::current_instance_id)"
     found_value="$(mageops::get_ec2_tag_value "$region" \
         "$instance_id" "$tag_name")"
 
