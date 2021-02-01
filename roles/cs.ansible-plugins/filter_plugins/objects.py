addb58025002eca8a4daa2d39563fc7e8470ffd6
Filip Sobalski <pinkeen@gmail.com>
2021-02-01 21:43:06 +0100

    [FEATURE] Allow using AWS EBS gp3 volumes instead of the default gp2 - ebs modify role

--- roles/cs.ansible-plugins/filter_plugins/objects.py
+++ roles/cs.ansible-plugins/filter_plugins/objects.py
@@ -29,6 +29,34 @@ def pick_prefixed_keys(object={}, prefix=''):
 def dict_to_tag_list(object={}):
     return [ {k: v} for k, v in object.items() ]
 
+def pick_nonempty_keys(object={}):
+    return { key: val for key, val in object.items() if bool(val) }
+
+# Returns list of keys that differ between two dicts - that are present in
+# either of them but not in both or have different value
+def dict_diff_keys(a={}, b={}):
+    return [ d[0] for d in set(a.items()).symmetric_difference(set(b.items())) ]
+
+# Same as dict_diff but returns a dict with changed values - the value from
+# first dict is used if key is present in both
+def dict_diff(has={}, had={}):
+    return { key: has[key] if key in has else had[key] for key, val in set(has.items()).symmetric_difference(set(had.items())) }
+
+def normalize_number(val):
+    try:
+        return int(val)
+    except ValueError:
+        pass
+
+    try:
+        return float(val)
+    except ValueError:
+        pass
+
+    return val
+
+def dict_normalize_numbers(object={}):
+    return { key: normalize_number(val) for key, val in object.items() }
 
 class FilterModule(object):
     def filters(self):
@@ -38,6 +66,10 @@ class FilterModule(object):
             'reject_keys': reject_keys,
             'prefix_keys': prefix_keys,
             'pick_prefixed_keys': pick_prefixed_keys,
-            'dict_to_tag_list': dict_to_tag_list
+            'dict_to_tag_list': dict_to_tag_list,
+            'pick_nonempty_keys': pick_nonempty_keys,
+            'dict_diff_keys': dict_diff_keys,
+            'dict_diff': dict_diff,
+            'dict_normalize_numbers': dict_normalize_numbers
         }
 
