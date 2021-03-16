a7487094607da27d6f8864a7dd18f15451a7e9f2
Filip Sobalski <pinkeen@gmail.com>
2021-03-16 14:05:19 +0100

    Some WIP stuff

--- roles/cs.ansible-plugins/filter_plugins/text.py
+++ roles/cs.ansible-plugins/filter_plugins/text.py
@@ -2,10 +2,14 @@ from __future__ import (absolute_import, division, print_function)
 
 __metaclass__ = type
 
-
 from ansible.errors import AnsibleError, AnsibleFilterError
-
 import re
+import textwrap
+
+try:
+    import sass
+except ImportError:
+    sass = None
 
 ''' Strip leading and trailing whitespace from each line
     while keeping newlines intact '''
@@ -15,10 +19,30 @@ def strip_lines(text, collapse=True):
         return text
     return re.sub('[\r\n]{2,}', '\n\n', text, flags=re.DOTALL)
 
+def sass_compile(**kwargs):
+    if not sass:
+        raise AnsibleError('Please install "libsass" python library on your controller in order to use this filter')
+
+    try:
+        return sass.compile(**kwargs)
+    except sass.CompileError as exc:
+        if 'filename' in kwargs:
+            raise AnsibleFilterError('Could not compile the scss file "%s":' % (filename, to_native(exc)))
+        # elif 'string' in kwargs:
+            # raise AnsibleFilterError('Could not compile the scss "%s":' % (textwrap.shorten(kwargs['string'], width=64, placeholder='[...]'), to_native(exc)))
+        else:
+            raise AnsibleFilterError('The scss compilation failed:' % to_native(exc))
+
+def scss_compile(string, output_style='nested'):
+    return sass_compile(string=string, output_style=output_style)
+
+def scss_compile_file(filename, output_style='nested'):
+    return sass.compile(filename=filename, output_style=output_style)
 
 class FilterModule(object):
     def filters(self):
         return {
             'strip_lines': strip_lines,
+            'scss_compile': scss_compile,
         }
 
