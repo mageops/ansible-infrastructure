4ba489e75f4f43f2fa0ae7f2419495eb9fb96b9e
Piotr Rogowski <piotr.rogowski@creativestyle.pl>
2021-12-14 12:51:47 +0100

    remove dependency for magento-facts in shel profile

--- roles/cs.mageops-cli-profile/templates/002-mageops-env.sh
+++ roles/cs.mageops-cli-profile/templates/002-mageops-env.sh
@@ -16,9 +16,5 @@ export MAGEOPS_MAGENTO_USER="{{ magento_user }}"
 export MAGEOPS_MAGENTO_GROUP="{{ magento_group }}"
 {% endif %}
 
-{% if magento_version is defined %}
-export MAGEOPS_MAGENTO_VERSION="{{ magento_version }}"
-{% endif %}
-
 export PS1="\[\e[0;35m\][${MAGEOPS_NODE_ROLE}.${MAGEOPS_PROJECT}-${MAGEOPS_ENVIRONMENT}]\[\e[0m\] "
 export PS1="${PS1}\[\e[0;33m\]\u\[\e[0;32m\]@\h\[\e[0m\] \[\e[0;94m\]\w\[\e[0m\] \[\e[1;32m\]→\[\e[0m\] "
