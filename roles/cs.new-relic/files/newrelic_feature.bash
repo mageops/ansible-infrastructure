17cb5b0037657a31282c955969f28c94cf9686bd
Mariusz Jóźwiak <mariusz.jozwiak@creativestyle.pl>
2024-04-09 10:55:38 +0200

    init

--- roles/cs.new-relic/files/newrelic_feature.bash
+++ roles/cs.new-relic/files/newrelic_feature.bash
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 feature__flag_name="newrelic_apm"
-feature__license_key="newrelic_license_key"
 
 feature::apply() {
     local expected
@@ -10,15 +9,9 @@ feature::apply() {
     expected="$(feature::expected_value)"
     expected="$(feature::normalize_expected "$expected")"
     current="$(feature::current_value)"
-    current_license="$(feature::current_license_value)"
-    expected_license="$(feature::expected_license_value)"
 
-    if [ -z "$expected_license" ];then
-        # We cannot enable the feature without a license key
-        expected="false"
-    fi
-    if [ "$expected" != "$current" ] || [ "$expected_license" != "$current_license" ];then
-        feature::update "$expected" "$expected_license"
+    if [ "$expected" != "$current" ];then
+        feature::update "$expected"
     fi
 }
 
@@ -41,10 +34,6 @@ feature::expected_value() {
     features::read_feature_flag "$feature__flag_name" "false"
 }
 
-feature::expected_license_value() {
-    features::read_feature_flag "$feature__license_key" ""
-}
-
 feature::current_value() {
     local current
 
@@ -57,21 +46,12 @@ feature::current_value() {
     echo "$current"
 }
 
-feature::current_license_value() {
-    local current
-
-    current="$(grep '^newrelic.license' /etc/php.d/newrelic.ini | sed 's/.*=\s*"\(.*\)"\s*$/\1/')"
-
-    echo "$current"
-}
-
 feature::update() {
     local value=$1
-    local license=$2
+    local config
 
     echo "Setting newrelic apm to $value"
     sed -i -e "s/newrelic.enabled[[:space:]]=[[:space:]].*/newrelic.enabled = ${value}/" /etc/php.d/newrelic.ini
-    sed -i -e "s/newrelic.license[[:space:]]=[[:space:]].*/newrelic.license = \"${license}\"/" /etc/php.d/newrelic.ini
     echo "Reloading php-fpm"
     systemctl reload php-fpm
 }
